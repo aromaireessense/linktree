24f2287b99c7f7c22212f5504756e6e4dadee6ac
aromaireessense <123017325+aromaireessense@users.noreply.github.com>
2023-01-22 02:33:00 +0700

    Update app.js
    diupdate tengah malam

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const buttonH3 = document.querySelectorAll("button h3");
+const capsH1 = document.querySelectorAll("div h1");
+const capsH2 = document.querySelectorAll("div h2");
 
 /* Node List of Buttons tag with h3 tag in it */
 console.log(buttonH3);
@@ -7,4 +9,24 @@ console.log(buttonH3);
 for(let buttonTitle of buttonH3) {
     console.log(buttonTitle.innerHTML.toUpperCase());
     buttonTitle.innerText = buttonTitle.innerHTML.toUpperCase();
+}
+
+/* Make the h1 and h2 become caps h1 */
+for(let titleCapsH1 of capsH1) {
+    console.log(titleCapsH1.innerHTML.toUpperCase());
+    titleCapsH1.innerText = titleCapsH1.innerHTML.toUpperCase();
+}
+
+/* Make the h1 and h2 become caps h2 */
+for(let titleCapsH2 of capsH2) {
+    console.log(titleCapsH2.innerHTML.toUpperCase());
+    titleCapsH2.innerText = titleCapsH2.innerHTML.toUpperCase();
+}
+
+/* Bikin String &amp; hilang menjadi '&' */
+function fixSymbolError(newFixH3Button) {
+    const fixH3Button = document.querySelectorAll("body");
+    let pattern = "jasmine";
+    let replacement = "jazz";
+    newFixH3Button = fixH3Button.replaceAll.innerHTML(pattern,replacement);
 }
